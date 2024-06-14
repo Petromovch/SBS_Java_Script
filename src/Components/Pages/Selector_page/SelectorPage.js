9ab850ef227b613dc16bd564b33639ecb1c58b4b
Markiianchess <hlovamarkiian@gmail.com>
2024-06-14 11:31:00 +0300

    index.css & Card_component

--- src/Components/Pages/Selector_page/SelectorPage.js
+++ src/Components/Pages/Selector_page/SelectorPage.js
@@ -1,25 +1,23 @@
 import style from "./SelectorPage.module.css"
 import human_image from "../../../images/human.svg"
 import computer_image from "../../../images/computer.svg"
+let Card = (props) => {
+    return(
+        <div className={style.card}>
+            <div className={style.card_image_container}>
+                <img src={props.image} alt={props.alt} className={style.card_image}></img>
+            </div>
+            <div className={style.card_title}>
+                {props.title}
+            </div>
+        </div>
+    )
+}
 let SelectorPage = () => {
     return(
         <div className={style.selector_page}>
-            <div className={style.card}>
-                <div className={style.card_image_container}>
-                    <img src={human_image} alt="Person" className={style.card_image}></img>
-                </div>
-                <div className={style.card_title}>
-                    1 VS 1
-                </div>
-            </div>
-            <div className={style.card}>
-                <div className={style.card_image_container}>
-                    <img src={computer_image} alt="Computer" className={style.card_image}></img>
-                </div>
-                <div className={style.card_title}>
-                   With Computer
-                </div>
-            </div>
+            <Card image={human_image} alt="Person" title="1 VS 1" />
+            <Card image={computer_image} alt="Computer" title="With Computer" />
         </div>
     )
 
